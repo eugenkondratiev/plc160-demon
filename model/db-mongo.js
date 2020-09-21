5276b8c982a39f6a36a6d1fe98742f97137236de
eugenkondratiev <kondratiev.es@gmail.com>
2020-09-21 12:10:27 +0300

    double data to local mongo

--- model/db-mongo.js
+++ model/db-mongo.js
@@ -2,21 +2,20 @@ const MongoClient = require("mongodb").MongoClient;
 const dotenv = require('dotenv');
 dotenv.config();
 
-const URI = process.env.MONGODB_URI;
-const DB_NAME = process.env.MONGODB_DB_NAME;
+const { MONGODB_URI, MONGODB_DB_NAME, MONGODB_LOCAL_URI, MONGODB_LOCAL_DB_NAME} = process.env;
 
 let cachedDb = null;
-module.exports = () => {
+module.exports = (_local=false) => {
   if (cachedDb && cachedDb._db && cachedDb._db.serverConfig.isConnected()) {
       console.log("###CONNECTED")
     return Promise.resolve(cachedDb);
   }
   
-  return MongoClient.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true  }).then(client => {
-    console.log(DB_NAME);
-    console.log(URI);
-
-    const dataBase = client.db(DB_NAME);
+  return MongoClient.connect(_local?MONGODB_LOCAL_URI : MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  }).then(client => {
+    // console.log(DB_NAME);
+    // console.log(URI);
+// 
+    const dataBase = client.db(_local ? MONGODB_LOCAL_DB_NAME : MONGODB_DB_NAME);
     return {_db :dataBase , client: client};
   });
 };
--- model/insert-hour-to-mongo.js
+++ model/insert-hour-to-mongo.js
@@ -1,39 +1,11 @@
 
 
 const Eco3_COLLECTION = "hourReportsEco3";
+const LOCAL_ECO3_COLLECTION = "Eco3";
 // const Eco3_DB = "Eco";
 // const { AVG, MIN, MAX } = require('../reports/report-constants');
 const fs = require('fs');
-
-// function formMongoRecord(reportManager) {
-//     const viewTable = reportManager._list.map(el => {
-//         return { [el.name + (el.type === MIN ? "min" : el.type === MAX ? "max" : "")]: el.par._lastHour }
-//     });
-//     const currentDateTime = new Date();
-//     const hh = currentDateTime.toLocaleTimeString("ru-UA", { hour: "2-digit"}).slice(0, 3) + ":00:00";
-//         return {
-//             _id: currentDateTime.toLocaleString("ru-UA", { year: "numeric", month: "2-digit", day: "2-digit" }).slice(0, 10) + " "
-//                 + hh,
-//             // + currentDateTime.toLocaleTimeString("ru-UA", { year: "numeric", month: "2-digit", day: "2-digit" }).slice(0, 3) + "00:00",
-//             values: viewTable.reduce((acc, el) => {
-//                 return { ...acc, ...el };
-//             }, {})
-//         }
-
-// }
-
-
-// function formSqlRecord(reportManager) {
-//     const hourRow = reportManager._list.map(el =>
-//         isFinite(el.par._lastHour) ? parseFloat(el.par._lastHour).toFixed(3) : "-"
-//     );
-//     const currentDateTime = new Date();
-//     const hh = currentDateTime.getHours();
-//     hourRow.unshift(currentDateTime.toLocaleString("ru-UA", { year: "numeric", month: "2-digit", day: "2-digit" }).slice(0, 10) + " "
-//         + (hh > 9 ? hh : ("0" + hh)) + ":00:00")
-//     // + currentDateTime.toLocaleTimeString().slice(0, 3) + "00:00")
-//     return hourRow;
-// }
+const LOCAL = true;
 
 async function insertNewHourRecordToMongo(_manager) {
 
@@ -42,27 +14,53 @@ async function insertNewHourRecordToMongo(_manager) {
         const newRecord = _manager.formMongoDbRecord();
         console.log("##DATA  newRecord ", newRecord);
         // console.log("##DATA  formSqlRecord ", _manager.formMySQLRecord());
-
-        try {
-            const mongoClient = await require('./db-mongo')();
-            // const collection = mongoClient._db.collection(Eco3_COLLECTION);
-            const data = await mongoClient._db.collection(Eco3_COLLECTION).updateOne(
-                { _id: newRecord._id },
-                {
-                    $set: {
-                        _id: newRecord._id,
-                        values: newRecord.values
-                    }
-                },
-                { upsert: true }
-            )
-            mongoClient.client.close();
-
-        } catch (error) {
-            console.log(" collection.updateOne problem ", error);
-            fs.writeFile('reserv/' + newRecord._id.replace(/:/g,'-'), JSON.stringify(newRecord, null, " "), (err => { if (err) console.error }))
+        const _query = { _id: newRecord._id }
+        const _update = {
+            $set: {
+                _id: newRecord._id,
+                values: newRecord.values
+            }
+        }
+        async function inserting(fb, _local = false) {
+            try {
+                const mongoClient = await require('./db-mongo')(_local);
+                // const collection = mongoClient._db.collection(Eco3_COLLECTION);
+                const data = await mongoClient._db.collection(_local ? LOCAL_ECO3_COLLECTION : Eco3_COLLECTION).updateOne(
+                    _query,
+                    _update,
+                    { upsert: true }
+                )
+                mongoClient.client.close();
+            } catch (error) {
+                // console.log(" collection.updateOne problem ", error);
+                // fs.writeFile('reserv/' + newRecord._id.replace(/:/g, '-'), JSON.stringify(newRecord, null, " "), (err => { if (err) console.error }))
+                fb(error)
+            }
         }
 
+        await inserting((error) => {
+            console.log(" collection.updateOne problem ", error);
+            fs.writeFile('reserv/' + newRecord._id.replace(/:/g, '-'), JSON.stringify(newRecord, null, " "), (err => { if (err) console.error }))
+        })
+        await inserting((error) => {
+            console.log(" Loacl MongoDbcollection.updateOne problem ", error);
+        },
+            LOCAL)
+
+        // try {
+        //     const mongoClient = await require('./db-mongo')();
+        //     // const collection = mongoClient._db.collection(Eco3_COLLECTION);
+        //     const data = await mongoClient._db.collection(Eco3_COLLECTION).updateOne(
+        //         _query,
+        //         _update,
+        //         { upsert: true }
+        //     )
+        //     mongoClient.client.close();
+
+        // } catch (error) {
+        //     console.log(" collection.updateOne problem ", error);
+        //     fs.writeFile('reserv/' + newRecord._id.replace(/:/g, '-'), JSON.stringify(newRecord, null, " "), (err => { if (err) console.error }))
+        // }
         // console.log("##DATA  insertNewHourRecordToMongo ", data);
     } catch (err) {
         console.log("#### FIND ERROR ", err);
